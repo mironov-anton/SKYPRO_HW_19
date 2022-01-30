3fa3cecf4852b102481f319e0f9aab0b83c2108c
mironov-anton <miron.khuzd@gmail.com>
2022-01-30 21:15:53 +0300

    hash functions for password handling added

--- config.py
+++ config.py
@@ -4,3 +4,6 @@ class Config(object):
     SQLALCHEMY_DATABASE_URI = 'sqlite:///./movies.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     # SERVER_NAME = 'localhost:10001'
+
+    PWD_HASH_SALT = '249y823r9v8238r9u'.encode("utf-8")
+    PWD_HASH_ITERATIONS = 100_000
